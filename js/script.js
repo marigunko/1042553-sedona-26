86b51d61cd633c6737fc38d6e7c20901d99d48e9
marigunko <35509767+marigunko@users.noreply.github.com>
2019-11-02 19:41:12 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var link = document.querySelector(".form-link");
+var popup = document.querySelector(".search-form");
+var close = popup.querySelector(".search-form");
+var form = popup.querySelector("form");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
+});
