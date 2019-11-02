515744d94509a06ea0acf52d70f0cff9bf751970
marigunko <35509767+marigunko@users.noreply.github.com>
2019-11-02 22:28:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ var popup = document.querySelector(".search-form");
 var close = popup.querySelector(".search-form");
 var form = popup.querySelector("form");
 
+var check-in-date = popup.querySelector("[name=check-in-date]");
+var сheck-out-date = popup.querySelector("[name=check-out-date]");
+var adults-number = popup.querySelector("[name=adults-number]");
+var children-number = popup.querySelector("[name=children-number]");
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
@@ -11,11 +16,20 @@ link.addEventListener("click", function (evt) {
   } else {
     popup.classList.add("hidden-form");
   }
+
 });
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
+
+form.addEventListener("submit", function (evt) {
+  if (!check-in-date.value || !check-out-date.value || !adults-number.value || !children-number.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+    console.log("Заполните все поля");
+  }
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
